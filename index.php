3ce498d717d3dfb0cacd0adf3831ae3fd2ce6dfd
Julia Evans <jevans8@mail.greenriver.edu>
2020-05-20 09:52:32 -0400

    Initial commit, default route

---
+++ index.php
@@ -0,0 +1,35 @@
+<?php
+
+//Julia Evans
+//Midterm
+//5-20-2020
+
+//Turn on error reporting
+ini_set('display_errors', 1);
+error_reporting(E_ALL);
+
+//Start a session
+//session_start();
+
+//Require the autoload file
+require_once('vendor/autoload.php');
+//require_once('model/data.php');
+
+//Instantiate the framework (Base class)
+$f3 = Base::instance();
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////
+//Default route
+$f3->route('GET /', function(){
+
+    echo "<h2>Midterm Survey</h2>";
+    echo "<a href=''>Take my Midterm Survey</a>";
+
+    //$view = new Template();
+    //echo $view->render('views/home.html');
+
+});
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////
+//Run the framework (fat free)
+$f3->run();
